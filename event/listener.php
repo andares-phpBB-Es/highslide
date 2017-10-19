79a9141c9473f203f06be42a3e7067700a0bfd9d
AlexSheer <alex_sheer@inbox.lv>
2017-10-19 19:11:53 +0300

    Create listener.php

---
+++ event/listener.php
@@ -0,0 +1,48 @@
+<?php
+/**
+*
+* @package * @package phpBB Extension - Highslide
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace bb3mobi\highslide\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+/**
+* Assign functions defined in this class to event listeners in the core
+*
+* @return array
+* @static
+* @access public
+*/
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.user_setup'					=> 'load_language_on_setup',
+		);
+	}
+
+/**
+* Constructor
+*/
+	public function __construct()
+	{
+	}
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'bb3mobi/highslide',
+			'lang_set' => 'highslide',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+}
